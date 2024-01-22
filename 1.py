ace1064b75e1ffa4f0085ea8afa0f0438c23075f
Ilya <esgis@bk.ru>
2024-01-22 21:14:07 +0400

    test

--- 1.py
+++ 1.py
@@ -2,3 +2,8 @@ for i in range (3):
     print('i=',i)
     for j in range (2):
         print('j=',j)
+        
+for i in range (3):
+    print('i=',i)
+    for j in range (2):
+        print('j=',j)
